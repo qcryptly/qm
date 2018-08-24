8e41eeed80f7324e24093ade13587da2035a9b95
qcryptly <qcryptly@gmail.com>
2018-08-24 03:02:30 -0600

    Got cuda tests to compile and pass

--- kernels/vector.spec.cxx
+++ kernels/vector.spec.cxx
@@ -2,12 +2,12 @@
 
 #include <qmtest.hxx>
 
-#include "vector.hxx"
+#include "vector.h"
 
 namespace QM {
 
 void set_values(float *&x, float *&y, int N) {
-  for (int i = 0; i < n; i ++) {
+  for (int i = 0; i < N; i ++) {
     x[i] = 1.0f;
     y[i] = 2.0f;
   }
@@ -19,8 +19,11 @@ TEST_F(Test, VectorTest) {
 
   Vectors::add(x, y, &set_values, N);
   float maxError = 0.0f;
-  for (int i = 0; i < N; i++)
-    maxError = fmax(maxError, fabs(y[i]-3.0f));
+  for (int i = 0; i < N; i++) {
+    if (maxError > y[i] - 3.0f + .001f) {
+      maxError = y[i];
+    }
+  }
   EXPECT_EQ(maxError, 0.0f);
   Vectors::free(x, y);
 }
